59c14f13fd3bb1218946f5e4beca28d26f4360b1
i2seys <ilya-savenkov.savenkov@yandex.ru>
2023-02-19 11:59:55 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/savenkov/temp/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.mirea.savenkov.temp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
---
+++ myapplication/src/main/java/ru/mirea/savenkov/myapplication/ButtonClicker.java
@@ -0,0 +1,38 @@
+package ru.mirea.savenkov.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ButtonClicker extends AppCompatActivity {
+
+    private TextView textViewOut;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_clicker);
+
+        textViewOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewOut.setText("Мой номер по списку №20");
+            }
+        };
+
+
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+    public void btnItIsNotMeClickListener(View view){
+        textViewOut.setText("Это не я сделал.");
+    }
+}
